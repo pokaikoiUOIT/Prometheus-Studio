e57953b5dfce85b89c83d429cde5d06e74b56307
100638204 <100638204@UOSL16-JW2KHC2>
2016-10-03 19:08:46 -0400

    COMMENTS

--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -4,11 +4,15 @@
 #include "stdafx.h"
 #include <iostream>
 
+using namespace
+
 
 int main()
 {
 	std::cout << "hello world\n";
 	std::cout << "it noel here\n";
+	cout << "THIS IS COOL!!";
+
 
 }
 
